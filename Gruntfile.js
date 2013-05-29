f417f4c3df014d50e4baa4aab5ea6ed7aa3193e2
Maurizio Mangione <granzebru@gmail.com>
2013-05-29 15:05:40 +0200

    added Grunt tasks for linting and minification

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: '<%= pkg.name %>.js',
+        dest: '<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      all: ['jr-highlight.js'],
+      options: {
+        'node': true,
+        'browser': true,
+        'bitwise': true,
+        'camelcase': true,
+        'curly': true,
+        'eqeqeq': true,
+        'immed': true,
+        'latedef': true,
+        'newcap': true,
+        'noarg': true,
+        'quotmark': 'single',
+        'regexp': true,
+        'undef': true,
+        'unused': true,
+        'trailing': true,
+        'smarttabs': true,
+        'jquery': true
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'jshint']);
+
+};
--- jr-highlight.js
+++ jr-highlight.js
@@ -17,35 +17,35 @@ Johann Burkard
 	$.fn.highlight = function(pat, ignore) {
 		function replaceDiacritics(str) {
 			var diacritics = [ [ /[\u00c0-\u00c6]/g, 'A' ],
-				[ /[\u00e0-\u00e6]/g, 'a' ], 
+				[ /[\u00e0-\u00e6]/g, 'a' ],
 				[ /[\u00c7]/g, 'C' ],
-				[ /[\u00e7]/g, 'c' ], 
+				[ /[\u00e7]/g, 'c' ],
 				[ /[\u00c8-\u00cb]/g, 'E' ],
-				[ /[\u00e8-\u00eb]/g, 'e' ], 
+				[ /[\u00e8-\u00eb]/g, 'e' ],
 				[ /[\u00cc-\u00cf]/g, 'I' ],
-				[ /[\u00ec-\u00ef]/g, 'i' ], 
+				[ /[\u00ec-\u00ef]/g, 'i' ],
 				[ /[\u00d1|\u0147]/g, 'N' ],
 				[ /[\u00f1|\u0148]/g, 'n' ],
 				[ /[\u00d2-\u00d8|\u0150]/g, 'O' ],
-				[ /[\u00f2-\u00f8|\u0151]/g, 'o' ], 
+				[ /[\u00f2-\u00f8|\u0151]/g, 'o' ],
 				[ /[\u0160]/g, 'S' ],
-				[ /[\u0161]/g, 's' ], 
+				[ /[\u0161]/g, 's' ],
 				[ /[\u00d9-\u00dc]/g, 'U' ],
-				[ /[\u00f9-\u00fc]/g, 'u' ], 
+				[ /[\u00f9-\u00fc]/g, 'u' ],
 				[ /[\u00dd]/g, 'Y' ],
 				[ /[\u00fd]/g, 'y' ]
 			];
-		
+
 			for ( var i = 0; i < diacritics.length; i++) {
 				str = str.replace(diacritics[i][0], diacritics[i][1]);
 			}
-			
+
 			return str;
 		}
-		
+
 		function innerHighlight(node, pat, ignore) {
 			var skip = 0;
-			if (node.nodeType == 3) {
+			if (node.nodeType === 3) {
 				var isPatternArray = $.isArray(pat);
 				if (!isPatternArray) {
 					pat = [pat];
@@ -65,7 +65,7 @@ Johann Burkard
 						skip = 1;
 					}
 				}
-			} else if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
+			} else if (node.nodeType === 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
 				for (var i = 0; i < node.childNodes.length; ++i) {
 					i += innerHighlight(node.childNodes[i], pat, ignore);
 				}
@@ -77,13 +77,13 @@ Johann Burkard
 			innerHighlight(this, pat, ignore);
 		}) : this;
 	};
-	
+
 	$.fn.highlight.defaults = {
 		ignore : false
-	}
+	};
 
 	$.fn.removeHighlight = function() {
-		return this.find("span.highlight").each(function() {
+		return this.find('span.highlight').each(function() {
 			this.parentNode.firstChild.nodeName;
 			with(this.parentNode) {
 				replaceChild(this.firstChild, this);
